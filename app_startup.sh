5f7aba801e60f1452b68bce9099f94e329005752
Jean Helou <jean.helou@gmail.com>
2014-06-24 09:34:53 +0200

    Allow remote configuration of JVM flags

--- app_startup.sh
+++ app_startup.sh
@@ -6,10 +6,21 @@ MAXWAIT=30
 function check {
   nc -vz localhost 9000 &>/dev/null
 }
-export NEWRELIC_OPTS="-Dnewrelic.environment=$APPLICATION_ENV -J-javaagent:$HOME/newrelic/newrelic.jar"
-export HAZELCAST_OPTS="-Dhazelcast.logging.type=slf4j -Dhazelcast.icmp.enabled=true"
+export APP_JVM_OPTS=""
+
+if [ -f $HOME/$APPLICATION_NAME/conf/jvm.sh ]; then
+  source $HOME/$APPLICATION_NAME/conf/jvm.sh
+fi 
+#export NEWRELIC_OPTS="-Dnewrelic.environment=$APPLICATION_ENV -J-javaagent:$HOME/newrelic/newrelic.jar"
+#export HAZELCAST_OPTS="-Dhazelcast.logging.type=slf4j -Dhazelcast.icmp.enabled=true"
 mkdir -p $APPLICATION_HOME/logs
-$APPLICATION_HOME/bin/$APPLICATION_NAME $NEWRELIC_OPTS $HAZELCAST_OPTS -Dapplication.home=$HOME/$APPLICATION_NAME/ -Dlogger.file=$HOME/$APPLICATION_NAME/conf/logger.xml -Dconfig.file=$HOME/$APPLICATION_NAME/conf/env.conf  &>$APPLICATION_HOME/logs/startup.log & 
+
+$APPLICATION_HOME/bin/$APPLICATION_NAME $APP_JVM_OPTS   \
+  -Dapplication.home=$HOME/$APPLICATION_NAME/           \
+  -Dlogger.file=$HOME/$APPLICATION_NAME/conf/logger.xml \
+  -Dconfig.file=$HOME/$APPLICATION_NAME/conf/env.conf   \
+  &>$APPLICATION_HOME/logs/startup.log & 
+
 COUNTER=0;
 while ! check && [ $COUNTER -lt $MAXWAIT ]  ;do
 sleep 5
--- get_config.sh
+++ get_config.sh
@@ -7,6 +7,10 @@ if [ -n "$VERSION" ]; then
 else
   APP_PATH=$APPLICATION_HOME
 fi
-aws s3 cp --region $S3_REGION $CONFIG_S3_BASEURL/$APPLICATION_ROLE/$APPLICATION_ENV/env.conf $APP_PATH/conf/env.conf
-aws s3 cp --region $S3_REGION $CONFIG_S3_BASEURL/$APPLICATION_ROLE/$APPLICATION_ENV/hazelcast.xml $APP_PATH/conf/hazelcast.xml
-aws s3 cp --region $S3_REGION $CONFIG_S3_BASEURL/$APPLICATION_ROLE/$APPLICATION_ENV/logger.xml $APP_PATH/conf/logger.xml
+
+S3_APPLICATION_PATH="$CONFIG_S3_BASEURL/$APPLICATION_ROLE/$APPLICATION_ENV/"a
+
+aws s3 cp --region $S3_REGION $S3_APPLICATION_PATH/env.conf $APP_PATH/conf/env.conf
+aws s3 cp --region $S3_REGION $S3_APPLICATION_PATH/hazelcast.xml $APP_PATH/conf/hazelcast.xml
+aws s3 cp --region $S3_REGION $S3_APPLICATION_PATH/logger.xml $APP_PATH/conf/logger.xml
+aws s3 cp --region $S3_REGION $S3_APPLICATION_PATH/jvm.sh $APP_PATH/conf/jvm.sh
