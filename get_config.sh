0b15cc618113528ec5072def09681197a0f590ca
EC2 Default User <ec2-user@ip-172-31-29-132.eu-west-1.compute.internal>
2014-06-26 15:51:33 +0000

    more generic config read

--- get_config.sh
+++ get_config.sh
@@ -10,7 +10,4 @@ fi
 
 S3_APPLICATION_PATH="$CONFIG_S3_BASEURL/$APPLICATION_ROLE/$APPLICATION_ENV/"
 
-aws s3 cp --region $S3_REGION $S3_APPLICATION_PATH/env.conf $APP_PATH/conf/env.conf
-aws s3 cp --region $S3_REGION $S3_APPLICATION_PATH/hazelcast.xml $APP_PATH/conf/hazelcast.xml
-aws s3 cp --region $S3_REGION $S3_APPLICATION_PATH/logger.xml $APP_PATH/conf/logger.xml
-aws s3 cp --region $S3_REGION $S3_APPLICATION_PATH/jvm.sh $APP_PATH/conf/jvm.sh
+aws s3 cp --recursive --region $S3_REGION $S3_APPLICATION_PATH $APP_PATH/conf
